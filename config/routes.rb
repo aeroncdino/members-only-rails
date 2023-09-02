b3ba003673218fcd55987b9c6b09f52c114aceab
aeroncdino <dinoaeron@gmail.com>
2023-09-02 19:26:40 +0800

    with models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :posts, only: [:new, :create, :index] do
+    before_action :authenticate_user!, only [:new, :create]
+  end
+
+  root "posts#index"
 end
