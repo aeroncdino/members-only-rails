25ddd844a3a724ce3384e0976dfe4b81ef2d0977
aeroncdino <dinoaeron@gmail.com>
2023-09-02 19:59:40 +0800

    with index and new html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+
+        if @post.save
+            redirect_to posts_path, notice: 'Post was successfully created.'
+          else
+            render :new
+          end
+
+    end
+
+    def index
+        @post = Post.all 
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
 end
